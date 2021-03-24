b93697e89e26d500044435e18c3ff57eed7c1dd2
MariaLaveniaVikaPamukasari <vikalavenia@gmail.com>
2021-03-24 09:32:45 +0700

    Membuat soal 2

---
+++ I0320056_soal2.py
@@ -0,0 +1,19 @@
+
+# Menghitung berapa kali angka kedua dapat dibagi menjadi angka pertama
+print("<<<<<<<MULAI>>>>>>>")
+print("Menghitung menggunakan operation floor")
+print("Note: Output program ini menunjukkan berapa kali angka kedua dapat dibagi menjadi angka pertama ")
+
+a = int(input("Masukkan data bilangan bulat pertama:"))
+b = int(input("Masukkan data bilangan bulat kedua:"))
+
+print("\nAngka pertama Anda:",a)
+print("Angka kedua Anda:",b)
+
+# Operation Floor
+ab = a // b
+print("\nHasil perhitungan Anda:")
+print("Angka pertama yang Anda input dapat dibagi sebanyak:",ab)
+print("<<<<<<<SELESAI>>>>>>>")
+
+
