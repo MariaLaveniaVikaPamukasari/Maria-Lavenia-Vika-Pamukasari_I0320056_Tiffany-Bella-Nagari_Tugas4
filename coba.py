6d7f5d5790ea0fde59a58a6e141c877ae13355a5
MariaLaveniaVikaPamukasari <vikalavenia@gmail.com>
2021-03-25 10:48:29 +0700

    membuat soal 3

--- coba.py
+++ coba.py
@@ -3,22 +3,23 @@
 print("\t\t\tSelamat datang di konter pemeriksaan berat bagasi Lion Air JT908")
 # Menginput berat dalam Kg
 # Mengubah lbs menjadi Kg
-a = 0.456
-b = float(a * 50)
-c = float(b + 110)
-d = float(49)
+a = 0.45
+b = (a * 60)
+c = b + 110
+d = 49
 print("\nBerat maksimum untuk 1 orang pernumpang adalah",b,"Kg")
 print("berat bagasi c:",c)
 print("berat bagasi d:",d)
-print('----------------------------------------------------')
+print('----------------------')
 # output: c > b is True
 print('c > b:',c > b)
-# output: d < b is False
-print('d < b:',d < b)
-print('----------------------------------------------------')
 print("Berat bagasi Anda melebihi batas maksimum")
 print("Silahkan mengurangi barang bawaan Anda atau membayar biaya tambahan")
+# output: d < b is False
+print('d < b:',d < b)
+print("Barang bawaan Anda tidak melebihi batas maksimum")
+print("Silahkan menuju ruang tunggu")
+
 print("\n\t\t\tTerimakasih atas kerja samanya")
 input("Tekan enter untuk kembali")
 #Selesai
-
